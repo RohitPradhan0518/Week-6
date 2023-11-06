2997d7f586b392efdc84e3d562865961b62a1779
Rohitpradhan <Rohitpradhan0518@gmail.com>
2023-11-06 17:35:40 -0600

    first commit

---
+++ arrow_function.js
@@ -0,0 +1,20 @@
+//NON-ARROW FUNCTION
+function doIt() {
+    console.log("Done.")
+}
+
+function addTwo(a, b) {
+    return a + b
+}
+
+console.log(addTwo(6, 7));
+
+// WHAT IS AN ARROW FUNCTION? ITS ANONYMOUS (WITHOUT A NAME), DOESNT *NEED* A NAME AKA LAMBDA
+const double = x => x * 2; // NAMING, PARENTHESES, return KEYWORD and CURLY BRACES ALL OPTIONAL
+console.log(double(11))
+
+const addThree = (a, b, c) => {
+    return a + b + c;
+}
+
+console.log(addThree(6,7,8))
